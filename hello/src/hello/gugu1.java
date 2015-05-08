bec7399f4870381b44cecdf5b1057a66fbb2d5b5
취업반 PM < PM@Itbank_05-PC>
2015-05-08 21:35:26 +0900

    구구단이라고 제목을 주었음..

--- hello/src/hello/gugu1.java
+++ hello/src/hello/gugu1.java
@@ -3,6 +3,7 @@ package hello;
 public class gugu1 {
 
 	public static void main(String[] args) {
+		System.out.println("±¸±¸´Ü");
 		for (int i = 2; i < 10; i += 4) {
 			for (int j = 1; j < 10; j++) {
 				System.out.println(
